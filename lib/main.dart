98d7063bfbe3b4e2ed644b3078529128ac2c7ba9
Jo-HyeRin <sayhyelin12@naver.com>
2022-11-22 12:24:18 +0900

    Firestore 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_firestore_stream/firebase_options.dart';
 import 'package:riverpod_firestore_stream/widget/pages/chat_room/chat_room_page.dart';
 
-void main() {
+void main() async {
+
+  // 메인 메서드에 비동기 작업이 있으면 꼭 필요하다.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 그림 그리기 전에 Firebase 실행.
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     // 위젯에서 프로바이더를 사용하고 읽기위해
     // 앱 전체적으로 "ProviderScope" 위젯을 감싸줘야 합니다.
