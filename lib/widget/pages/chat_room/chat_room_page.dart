7a9b1f9f84db685a8442bae7179cb51669a2e81b
Jo-HyeRin <sayhyelin12@naver.com>
2022-11-22 10:39:40 +0900

    chatroompage body ListView 적용

--- lib/widget/pages/chat_room/chat_room_page.dart
+++ lib/widget/pages/chat_room/chat_room_page.dart
@@ -7,6 +7,18 @@ class ChatRoomPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
+      body: _buildListView(),
+    );
+  }
+
+  ListView _buildListView() {
+    return ListView.separated(
+      itemCount: 2,
+      itemBuilder: (context, index) => ListTile(
+        title: Text("msg: 보낸메세지내용"),
+        subtitle: Text("from: 보낸사람"),
+      ),
+      separatorBuilder: (context, index) => Divider(),
     );
   }
 
